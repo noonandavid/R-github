5ce389ca98c6ae3bea82bb510ffb6b089f92bb61
noonandavid <noonanda@tcd.ie>
2017-11-28 08:01:34 +0000

    28/11/17

--- R github.R
+++ R github.R
@@ -1,10 +1,10 @@
 library("httpuv", lib.loc="/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
 library("httr", lib.loc="/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
 library("jsonlite", lib.loc="/Library/Frameworks/R.framework/Versions/3.3/Resources/library")
-
+#5e0f7bbfcc96ace79a7e1b2b3e5b89b1d9a81612 
 info  <- fromJSON("https://api.github.com/users/torvalds")
 e  = 1
-list_of_follower <- c("https://api.github.com/users/torvalds/followers")
+list_of_followers <- c("https://api.github.com/users/torvalds/followers")
 list_of_reps <- c("https://api.github.com/users/torvalds/repos")
 while (e != 2){
  i = 1
@@ -12,14 +12,15 @@ while (e != 2){
   b = "?page="
   c = "&per_page=100'"
   url1 <- paste0(a, b, i, c)
-  json <-fromJSON(url1)
+  web <- GET(url1)
+  json <-fromJSON(web)
   repurl <- json$repos_url
   folurl <- json$followers_url
-  if (length(folurl) == 0 ){e = 2}
+  #if (length(folurl) == 0 ){e = 2}
   list_of_followers <- c(list_of_followers,folurl)
-  list_of_reps <- c(list_of_followers,folurl)
-  
-    
+  list_of_reps <- c(list_of_reps,repurl)
   i <- i+1
+  list_of_followers[!duplicated(list_of_followers) ]
 }
-x
+web
+?GET
